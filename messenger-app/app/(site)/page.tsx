2e4490b899ee472e8ce9ce84321a14cef2ac3890
ncls-brn <nicolas.j.baran@gmail.com>
2023-05-19 16:02:35 +0200

    crestion components and authform

--- messenger-app/app/(site)/page.tsx
+++ messenger-app/app/(site)/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import AuthForm from "@/app/(site)/components/AuthForm";
 
 export default function Home() {
   return (
@@ -32,7 +33,7 @@ export default function Home() {
           Sign in to your account
         </h2>
       </div>
-      {/* Authform */}
+      <AuthForm/>
     </div>
   );
 }
---
+++ messenger-app/app/components/inputs/Input.tsx
@@ -0,0 +1,35 @@
+'use client';
+import clsx from "clsx";
+import { FieldErrors,FieldValues, UseFormRegister } from "react-hook-form";
+
+
+interface InputProps{
+    label:string;
+    id:string;
+    type?:string;
+    required?:boolean;
+    register:UseFormRegister<FieldValues>,
+    errors:FieldErrors,
+    disabled?:boolean;
+}
+
+
+
+const Input: React.FC<InputProps> =({
+    label,
+    id,
+    type,
+    required,
+    register,
+    errors,
+    disabled
+
+}) => {
+    return(
+        <div>
+            I'm a input! 
+        </div>
+    )
+}
+
+export default Input
