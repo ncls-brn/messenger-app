7b41e6fc643bb9073ef6db1e12e727606e6e3296
ncls-brn <nicolas.j.baran@gmail.com>
2023-05-23 10:06:42 +0200

    creation button auth social

--- messenger-app/app/(site)/components/AuthForm.tsx
+++ messenger-app/app/(site)/components/AuthForm.tsx
@@ -4,6 +4,7 @@ import Button from "@/app/components/Button";
 import Input from "@/app/components/inputs/Input";
 import { useCallback, useState } from "react";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import AuthSocialButton from "./AuthSocialButton";
 
 type Variant = "LOGIN" | "REGISTER";
 
@@ -66,22 +67,74 @@ const AuthForm = () => {
             sm:px-10"
       >
         <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
-            {variant == 'REGISTER' && (
-          <Input id="name" label="Name" register={register} errors={errors} />)}
-          <Input id="email" label="Email address" type="email" register={register} errors={errors} />
-          <Input id="password" label="Password" type="password" register={register} errors={errors} />
+          {variant == "REGISTER" && (
+            <Input id="name" label="Name" register={register} errors={errors} />
+          )}
+          <Input
+            id="email"
+            label="Email address"
+            type="email"
+            register={register}
+            errors={errors}
+          />
+          <Input
+            id="password"
+            label="Password"
+            type="password"
+            register={register}
+            errors={errors}
+          />
           <div>
-            <Button
-                disabled={isLoading}
-                fullWidth
-                type="submit"
-            >
-
-                {variant == 'LOGIN' ? 'Sign in': 'Register'}
-
+            <Button disabled={isLoading} fullWidth type="submit">
+              {variant == "LOGIN" ? "Sign in" : "Register"}
             </Button>
           </div>
         </form>
+        <div className="mt-6">
+          <div className="relative">
+            <div
+              className="
+                    absolute
+                     inset-0
+                     flex
+                     items-center
+                     "
+            >
+              <div
+                className="w-full
+                         border-t
+                          border-gray-300
+                          "
+              >
+                <div
+                  className="
+                                relative
+                                flex
+                                justify-center
+                                text-sm
+                                "
+                >
+                  <span
+                    className="
+                                        bg-white
+                                        px-2 
+                                        text-gray-500
+                                        "
+                  >
+                    Or continue with
+                  </span>
+                  
+                </div>
+              </div>
+
+              
+            </div>
+            
+          </div>
+          <div className="mt-8 flex gap-2">
+                <AuthSocialButton />
+              </div>
+        </div>
       </div>
     </div>
   );
