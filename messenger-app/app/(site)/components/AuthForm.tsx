4612c23989a881f46f8fe67e456572c89aa76eb5
ncls-brn <nicolas.j.baran@gmail.com>
2023-06-02 11:53:37 +0200

    schema prisma done

--- messenger-app/app/(site)/components/AuthForm.tsx
+++ messenger-app/app/(site)/components/AuthForm.tsx
@@ -69,7 +69,7 @@ const AuthForm = () => {
       >
         <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
           {variant == "REGISTER" && (
-            <Input id="name" label="Name" register={register} errors={errors} />
+            <Input id="name" label="Name" register={register} errors={errors} disabled={isLoading}/>
           )}
           <Input
             id="email"
@@ -77,6 +77,7 @@ const AuthForm = () => {
             type="email"
             register={register}
             errors={errors}
+            disabled={isLoading}
           />
           <Input
             id="password"
@@ -84,6 +85,7 @@ const AuthForm = () => {
             type="password"
             register={register}
             errors={errors}
+            disabled={isLoading}
           />
           <div>
             <Button disabled={isLoading} fullWidth type="submit">
